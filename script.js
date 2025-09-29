54e5e2f061864a826a21c689f9b1999c51e9517a
SATHISH B <sathishb2310@gmail.com>
2025-09-29 15:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 function firstWord(str) {
   if (str.indexOf(' ') === -1) {
-    return str;
+    return str.trim(); // Return the trimmed string if there's no space
+  } else {
+    return str.slice(0, str.indexOf(' ')).trim(); // Extract the first word
   }
-  // ... rest of the code
 }
 const s = prompt("Enter String:");
 alert(firstWord(s));
