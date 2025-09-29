9242b09f6b1ffa983631e74a332c21c7b7860eb9
SATHISH B <sathishb2310@gmail.com>
2025-09-29 15:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function firstWord(str) {
-    if(str.indexOf(' ') === -1){
-	   return str;
-}
-}
 
+function firstWord(str) {
+  if (str.indexOf(' ') === -1) {
+    return str;
+  }
+  // ... rest of the code
+}
 const s = prompt("Enter String:");
 alert(firstWord(s));
